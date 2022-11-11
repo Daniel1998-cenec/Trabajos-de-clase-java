c24f4ed1505573ed671cc5bb8ca89bac0ec1e489
marllanesgarcia <115066302+Daniel1998-cenec@users.noreply.github.com>
2022-11-11 14:32:30 +0100

    PFFFFFFFFFFFFFF

--- jugando.con.strings.dam1/src/jugandoconstringsdam/JugandoConStrings.java
+++ jugando.con.strings.dam1/src/jugandoconstringsdam/JugandoConStrings.java
@@ -8,48 +8,61 @@ public class JugandoConStrings {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		char opcion = '-';
-		String texto="";
+		String texto = "";
 		while (opcion != 's') {
-			System.out.println("Elige una accion:" 
-		+ "\n\te - Escribir texto" + "\n\ti - Invertir el texto"			
-		+ "\\n\tc - Codificar texto" + "\n\tf - Mimificar un texto" 
-		+ "\n\td - Decodificar el texto"
-		+ "\n\tM - Cambiar a todo mayúsculas" + "\n\tm - Cambiar a todo minúsculas"
-		+ "\n\tn - Poner mayúsculas solo la primera letra" 
-		+ "\n\ta - Codificar con números aleatorios"+"\n\t0 - Dime si el texto tiene números"
-		
+			System.out.println("Elige una accion:" + "\n\te - Escribir texto" + "\n\ti - Invertir el texto"
+					+ "\n\tc - Codificar texto" + "\n\tf - Mimificar un texto" + "\n\td - Decodificar el texto"
+					+ "\n\tM - Cambiar a todo mayúsculas" + "\n\tm - Cambiar a todo minúsculas"
+					+ "\n\tn - Poner mayúsculas solo la primera letra" + "\n\ta - Codificar con números aleatorios"
+					+ "\n\t0 - Dime si el texto tiene números"
+
 					+ "\n\ts - Salir");
 			opcion = sc.nextLine().charAt(0);
 			switch (opcion) {
 			case '0':
-				System.out.println("¿Tiene el texto número?"+(texto.contains("1")||texto.contains("2")||texto.contains("3")||texto.contains("4")||
-						texto.contains("5")||texto.contains("6")||texto.contains("7")||texto.contains("8")||texto.contains("9")||texto.contains("0")));
+				System.out.println("¿Tiene el texto número?" + (texto.contains("1") || texto.contains("2")
+						|| texto.contains("3") || texto.contains("4") || texto.contains("5") || texto.contains("6")
+						|| texto.contains("7") || texto.contains("8") || texto.contains("9") || texto.contains("0")));
 				break;
 			case 'e':
 				System.out.println("voy escribir un texto");
-				texto=sc.nextLine();
-				
+				texto = sc.nextLine();
+
 				break;
 			case 'i':
 				System.out.println("Voy a invertir un texto");
+				for (byte i = (byte) (texto.length() - 1); i >= 0; i--) { // (byte i=0 ;i <texto.length(); i++)
+																			// Incrementa !!! // (byte
+																			// i=(byte)(texto.lenght()-1) ;i>=0 ;i--) Lo
+																			// inviertes!!!! (decrece)
+					System.out.print(texto.charAt(i));
+
+				}
+				System.out.println();
 				break;
 			case 'c':
 				System.out.println("Voy a codificar un texto");
 				break;
 			case 'f':
 				System.out.println("Voy a Mimificar un texto");
+				//hola
+				//hmiomilmiami
+				for (byte i=0;i<texto.length();i++) {
+					System.out.print(texto.charAt(i)+"mi");
+				}
+				System.out.println();
 				break;
 			case 'd':
 				System.out.println("Voy decodificar el texto");
 				break;
 			case 'M':
 				System.out.println("Voy a cambiar a todo mayúsculas");
-				texto=texto.toUpperCase();
+				texto = texto.toUpperCase();
 				System.out.println(texto);
 				break;
 			case 'm':
 				System.out.println("Voy a cambiar a todo minúsculas");
-				texto=texto.toLowerCase();
+				texto = texto.toLowerCase();
 				System.out.println(texto);
 				break;
 			case 'n':
