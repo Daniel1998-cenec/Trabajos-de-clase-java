ef6d53fb8ece407572dcd918d4c36853f8c4f87b
Daniel1998-cenec <danielcesar1998@hotmail.com>
2022-11-14 14:35:57 +0100

    .

---
+++ prueba.funciones.dam/src/main/java/prueba/funciones/dam/PruebasFunciones.java
@@ -0,0 +1,39 @@
+package prueba.funciones.dam;
+
+import java.util.Scanner;
+
+public class PruebasFunciones {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Dime cuantos pisos quieres que tenga las piramides");
+		byte pisos = Byte.parseByte(sc.nextLine());
+		byte i;
+		for (i = 0; i < pisos; i++) {
+			for (byte j = 0; j < (i + 1); j++) {
+				System.out.println("@");
+			}
+			System.out.println();
+		}
+		
+		for (byte p= (byte)(pisos - 1); p > 0; p--) {
+			for (byte j=0; j< (i + 1); j++) {
+				System.out.println("@");
+			}
+			System.out.println();
+
+			/*
+			 * System.out.println("Dime un número. y te digo si es primo"); long numero =
+			 * Long.parseLong(sc.nextLine()); boolean esPrimo = true; long i; for (i=2; i <
+			 * numero; i++) { if (numero % i == 0) { System.out.println("Lo divide "+i);
+			 * esPrimo = false; break; //Se usa break cuando para ahorrate tiempo } for(int
+			 * j=0;j<10;j++) {
+			 * 
+			 * } }
+			 * 
+			 * if (esPrimo) { System.out.println("Es primo"); } else {
+			 * //System.out.println("No es primo, lo divide "+i); }
+			 */
+		}
+	}
+}
