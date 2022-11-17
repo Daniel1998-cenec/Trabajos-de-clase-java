95d8e281734f4e87a25f14fa5b60f8239cd3dc4b
Daniel1998-cenec <danielcesar1998@hotmail.com>
2022-11-17 19:16:21 +0100

    .

--- Tamagochi1/src/main/java/com/dvargas/tamagochi/dam/TamagochiSupremo.java
+++ Tamagochi1/src/main/java/com/dvargas/tamagochi/dam/TamagochiSupremo.java
@@ -1,41 +1,50 @@
 package com.dvargas.tamagochi.dam;
 
+import java.io.InputStream;
 import java.util.Scanner;
 
 public class TamagochiSupremo {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		String nombre;
-		char raza = '-';
-		//System.out.println("¿que nombre tiene?");
-		
-
-		while (raza>1 && raza<4) {
-			System.out.println("¿Qué raza tiene el tamagochi?");
+		byte razasTamagochi=0;
+		String razas;
+		while (razasTamagochi<1||razasTamagochi>4) {
+			      
+			System.out.println("Dime la raza de tu tamagochi");
 			System.out.println("Elige la raza:");
-			System.out.println("\n\t1. Perro" + "\n\t2. Gato" + "\n\t3. Hipopótamo"+ "\n\t4. Carpincho"); 
-			raza = scan.nextLine().charAt(0);
-			
-		switch (raza) {
-		case 1:
-			System.out.println("Eres un perro");
-			break;
-		case 2: 
-			System.out.println("Eres un gato");
-			break;
-		case 3:
-			System.out.println("Eres un hipopotamo");
-			break;
-		case 4: 
-			System.out.println("Eres un Carpincho");
-		default:
-			System.out.println("Tenías que elegir entre 1 y 4");
-			break;
-			
-		
-		}
+			System.out.println("\n\t1. Perro" + "\n\t2. Gato" + "\n\t3. Hipopótamo" + "\n\t4. Carpincho");
+			razasTamagochi=Byte.parseByte(scan.nextLine());
 
+			switch (razasTamagochi) {
+			case 1:
+				System.out.println("Tu tamagochi es un perro");
+				break;
+			case 2:
+				System.out.println("Tu tamagochi es un gato");
+				break;
+			case 3:
+				System.out.println("Tu tamagochi es un hipopotamo");
+				break;
+			case 4:
+				System.out.println("Tu tamagochi es un Carpincho");
+			default:
+				System.out.println("Tenías que elegir entre 1 y 4");
+				break;
+			}
+		}
+		
+		System.out.println("Dime el nombre de tu tamagochi");
+		String nombre=scan.nextLine();
+		System.out.println("Tu tamagotchi tendrá las siguientes propiedades. Todas empiezan por defecto en 50:");
+		System.out.println("\n\ta - Hambre: entre 0 y 100. Con 0 se muere de hambre, con 100, está lleno."
+				+ "\n\tb - Sueño: entre 0 y 100. Con 0 se muere de insomnio, con 100, está descansado."
+				+ "\n\tc - Higiene: entre 0 y 100. Con 0, se muere bajo el peso de su propia mugre. Con 100 está limpio."
+				+ "\n\td - Diversión:entre 0 y 100. Con 0, se muere deprimido. Con 100 está pletórico."
+				+ "\n\te - (SOLO EL PERRO) Paseo: entre 0 y 100. Con 0 se muere por no salir a la calle. Con 100 está satisfecho."
+				+ "\n\tf - (SOLO EL CARPINCHO) Tomar mate : Entre 0 y 100. Con 0 se muere de sindrome de abstinencia. Con  100, está satisfecho.");
+		
+		
 	}
-}
+
 }
---
+++ jugando.con.strings.dam1/src/jugandoconstringsdam/JugandoConStrings.java
@@ -0,0 +1,152 @@
+package jugandoconstringsdam;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class JugandoConStrings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		char opcion = '-';
+		String texto = "";
+		String mimificado = "";
+		String codificado = "";
+		String descodificado = "";
+		Random aleatorio= new Random();
+		
+
+		while (opcion != 's') {
+			System.out.println("Elige una accion:" + "\n\te - Escribir texto" + "\n\ti - Invertir el texto"
+					+ "\n\tc - Codificar texto" + "\n\tf - Mimificar un texto" + "\n\td - Decodificar el texto"
+					+ "\n\tM - Cambiar a todo mayúsculas" + "\n\tm - Cambiar a todo minúsculas"
+					+ "\n\tp - desmimificar un texto" + "\n\tn - Poner mayúsculas solo la primera letra"
+					+ "\n\ta - Codificar con números aleatorios" + "\n\t0 - Dime si el texto tiene números"
+
+					+ "\n\ts - Salir");
+
+			opcion = sc.nextLine().charAt(0);
+			switch (opcion) {
+			case '0':
+				System.out.println("¿Tiene el texto número?" + (texto.contains("1") || texto.contains("2")
+						|| texto.contains("3") || texto.contains("4") || texto.contains("5") || texto.contains("6")
+						|| texto.contains("7") || texto.contains("8") || texto.contains("9") || texto.contains("0")));
+				break;
+			case 'e':
+				System.out.println("voy escribir un texto");
+				texto = sc.nextLine();
+
+				break;
+
+			case 'i':
+				System.out.println("Voy a invertir un texto");
+				for (byte i = (byte) (texto.length() - 1); i >= 0; i--) { // (byte i=0 ;i <texto.length(); i++)
+																			// Incrementa !!! // (byte
+																			// i=(byte)(texto.lenght()-1) ;i>=0 ;i--) Lo
+																			// inviertes!!!! (decrece)
+					System.out.print(texto.charAt(i));
+
+				}
+				System.out.println();
+				break;
+			case 'c':
+				System.out.println("Voy a codificar un texto");
+				for (byte i = 0; i < texto.length(); i++) {
+					codificado += (char) (texto.charAt(i) + 10);
+				}
+				System.out.println(codificado);
+
+				break;
+			case 'f':
+				System.out.println("Voy a Mimificar un texto");
+				// hola
+				// hmiomilmiami
+
+				for (byte i = 0; i < texto.length(); i++) {
+					// 1 - En vez de imprimir, concatenar en la variable mimificado
+
+					mimificado += (texto.charAt(i) + "mi"); // System.out.print(texto.charAt(i)+"mi");
+				}
+				// 2 - Imprimir la variable mimificado
+
+				System.out.println(mimificado);
+
+				break;
+
+			case 'p':
+				// 3- Hacer otro for que a partir de la variable mimificado,imprima la palabra
+				// original
+				String desmimificar = " ";
+				for (byte i = 0; i < mimificado.length(); i = (byte) (i + 3)) {
+					System.out.print(mimificado.charAt(i));
+				}
+				System.out.println();
+
+				mimificado = mimificado.replace("mi ", "");
+				System.out.println(mimificado);
+
+			case 'd':
+				System.out.println("Voy decodificar el texto");
+				for (byte i = 0; i < codificado.length(); i++) {
+					descodificado += (char) (codificado.charAt(i) - 10);
+				}
+				System.out.println(descodificado);
+
+				break;
+			case 'M':
+				System.out.println("Voy a cambiar a todo mayúsculas");
+				texto = texto.toUpperCase();
+				System.out.println(texto);
+				break;
+			case 'm':
+				System.out.println("Voy a cambiar a todo minúsculas");
+				texto = texto.toLowerCase();
+				System.out.println(texto);
+				break;
+			case 'n':
+				System.out.println("Voy a poner mayúsculas solo la primera letra");
+				System.out.print(("" + texto.charAt(0)).toUpperCase());
+
+				for (byte i = 1; i < texto.length(); i++) {
+					System.out.print(("" + texto.charAt(i)).toLowerCase());
+				}
+
+				System.out.println();
+
+				break;
+			case 'a':
+				System.out.println("Voy a codificar con números aleatorios");
+
+				// Haz aqui lo mismo q la opcion c
+				// pero ademas de codificar entre letra y letra introduce
+				// una letra a y z aleatoria.
+				// Eje, si escribimos en texto: acaba, la opcion c daria:
+				// kmklk
+				// Esta opción, además de eso, entre letra y letrainserta
+				// otra letra aleatoria. Eje:
+				// kvmxkrlqku
+				
+				
+				for (byte i = 0; i < texto.length(); i++) {
+				
+					codificado += (char)(texto.charAt(i) + 10);
+					codificado+= (char)(aleatorio.nextInt(97,123));	
+				}
+				
+				System.out.println(codificado);
+				
+				break;
+				
+			case 's':
+				System.out.println("Te me calmas, vete usted tranquilo con Dios");
+				break;
+			default:
+				System.out.println("La opcion ' " + opcion + " ' no es correcta");
+			}
+
+			System.out.println("Pulsa intro para continuar, y te me calmas");
+			sc.nextLine();
+		}
+
+	}
+}
