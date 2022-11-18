9843bb264a1435b51d97cd37303c9de0fb70dfff
Daniel1998-cenec <danielcesar1998@hotmail.com>
2022-11-18 19:15:50 +0100

    .

--- Tamagochi1/src/main/java/com/dvargas/tamagochi/dam/TamagochiSupremo.java
+++ Tamagochi1/src/main/java/com/dvargas/tamagochi/dam/TamagochiSupremo.java
@@ -25,7 +25,7 @@ public class TamagochiSupremo {
 
 		while (razasTamagochi < 1 || razasTamagochi > 4) {
 
-			System.out.println("Dime la raza de tu tamagochi");
+			System.out.println("Dime la raza de tu tamagochi"+"\n");
 			System.out.println("Elige la raza:");
 			System.out.println("\n\t1. Perro" + "\n\t2. Gato" + "\n\t3. Hipopótamo" + "\n\t4. Carpincho");
 			razasTamagochi = Byte.parseByte(scan.nextLine());
@@ -60,23 +60,30 @@ public class TamagochiSupremo {
 				|| nombre.contains("5") || nombre.contains("6") || nombre.contains("7") || nombre.contains("8")
 				|| nombre.contains("9") || nombre.contains("0")));
 
-		System.out.println(nombre + " " + razas + "\n" + "\n\t1 - Hambre: " + hambre + "\n\t2 - Sueño: " + sueño
-				+ "\n\t3 - Higiene: " + higiene + "\n\t4 - Diversion: " + diversion);
+		System.out.println(nombre + " " + razas + "\n"
+				+ "\n\t1 - Hambre: "+hambre 
+				+ "\n\t2 - Sueño: " + sueño
+				+ "\n\t3 - Higiene: " + higiene 
+				+ "\n\t4 - Diversion: " + diversion);
 
 		if (razas.equalsIgnoreCase("perro")) {
-			System.out.println("\t5 - paseo: " + paseo);
+			System.out.print("\t5 - paseo: " + paseo+"\n");
 		}
 		if (razas.equalsIgnoreCase("carpincho")) {
-			System.out.println("\t5 - tomarmate: " + tomarMate);
+			System.out.print("\t5 - tomarmate: " + tomarMate+"\n");
 		}
-
-		System.out.println("Elige acción:" + "\n" + "1 - Comer\n" + "2 - Dormir\n" + "3 - Ducharse\n" + "4 - Jugar\n");
+		
+		System.out.println("\nElige acción:\n"+ 
+				"\n\t1 - Comer" + 
+				"\n\t2 - Dormir" + 
+				"\n\t3 - Ducharse" + 
+				"\n\t4 - Jugar");
 
 		if (razas.equals("perro")) {
-			System.out.println("\t5 - paseo: " + paseo);
+			System.out.print("\t5 - Paseo "+"\n");
 		}
 		if (razas.equals("carpincho")) {
-			System.out.println("\t5 - tomarmate: " + tomarMate);
+			System.out.print("\t5 - Tomarmate "+"\n");
 		}
 
 		byte opcion = Byte.parseByte(scan.nextLine());
