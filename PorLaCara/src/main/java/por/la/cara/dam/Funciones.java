f98a63a27850a0a6e432045d689d1da8a9c77b71
marllanesgarcia <115066302+Daniel1998-cenec@users.noreply.github.com>
2022-11-21 14:28:49 +0100

    .

--- PorLaCara/src/main/java/por/la/cara/dam/Funciones.java
+++ PorLaCara/src/main/java/por/la/cara/dam/Funciones.java
@@ -9,3 +9,8 @@ public class Funciones {
 		return ret;
 	}
 }
+//Haz una funcion q devuelva un string y reciba por argumentos un string llamado trozos
+//y un char llamado reemplazo. DEntro de la funcion usa la funcion replace para cambiar todos los 'o'. que 
+//haya en el string trozos por el char reemplazo
+//EJ.Si llamo a funciones. reemplazar ("(|o o|)", 'x')
+//Que me devuelva el string "(|x x|)"
