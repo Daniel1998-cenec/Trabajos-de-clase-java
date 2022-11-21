2480de9d098822407cf8706ce11f57ac7e81e375
marllanesgarcia <115066302+Daniel1998-cenec@users.noreply.github.com>
2022-11-21 12:20:01 +0100

    .

---
+++ EjercicioPercebe1Dam/src/main/java/com/dvargas/percebes/dam/PercebesPrincipal.java
@@ -0,0 +1,34 @@
+package com.dvargas.percebes.dam;
+
+import java.util.Scanner;
+
+public class PercebesPrincipal {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Dime cuantos bloques quieres");
+		byte nb=Byte.parseByte(sc.nextLine());
+		System.out.println("Dime cuantos pisos quieres");
+		byte np=Byte.parseByte(sc.nextLine());
+		
+		System.out.println("|----------------------------|\n"
+				          +"| 13 Rue del Percebe |\n"
+				          + "|----------------------------|");
+		
+		for (byte c=0; c<np; c++) {   
+			
+		for (byte d=0; d<nb; d++) {   
+			System.out.print("|-----|");
+		}
+		System.out.println();
+		for ( byte a= 0;  a< np; a++) {
+			for (byte b =0; b<nb; b++) {  
+			System.out.print("|     |");
+			}
+			System.out.println();
+			
+		}	
+		}
+	}
+
+}
