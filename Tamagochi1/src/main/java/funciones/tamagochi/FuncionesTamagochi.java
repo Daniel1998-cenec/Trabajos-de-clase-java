5cc192ca7b5f205d6539cebdc9df04d64edf7240
marllanesgarcia <115066302+Daniel1998-cenec@users.noreply.github.com>
2022-11-22 12:01:32 +0100

    .

---
+++ Tamagochi1/src/main/java/funciones/tamagochi/FuncionesTamagochi.java
@@ -0,0 +1,21 @@
+package funciones.tamagochi;
+
+public class FuncionesTamagochi {
+
+ public static String funcionesTamagochi (String razasTamagochi) {
+	 
+	(razasTamagochi < 1 || razasTamagochi > 4) {
+		
+		System.out.println("Dime la raza de tu tamagochi" + "\n");
+		System.out.println("Elige la raza:"+" (Mediante un número)");
+		System.out.println("\n\t1. Perro" + "\n\t2. Gato" + "\n\t3. Hipopótamo" + "\n\t4. Carpincho");
+
+
+}
+ 
+	return razasTamagochi;
+	 
+ 	}
+	
+}
+
