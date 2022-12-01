631c515c38047d3857e0c02c92911dfc9e2d9c39
Daniel1998-cenec <danielcesar1998@hotmail.com>
2022-12-01 23:29:29 +0100

    .

---
+++ repaso.examen.opcional/src/main/java/repaso/examen/opcional/Principal.java
@@ -0,0 +1,21 @@
+package repaso.examen.opcional;
+
+import java.util.Scanner;
+
+public class Principal {
+//Ejercicio 7.
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String texto = "";
+		System.out.println("Dame un texto, por favor");
+		texto = sc.nextLine();
+		System.out.println("Dame dos numeros, por favor");
+		byte posicionInicial = Byte.parseByte(sc.nextLine());
+		byte posicionFinal = Byte.parseByte(sc.nextLine());
+
+		do {
+
+		} while (posicionInicial < posicionFinal) > texto.length();
+	}
+
+}
