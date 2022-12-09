12c1f79405d4191282537e8b1586b1514c4280de
marllanesgarcia <115066302+Daniel1998-cenec@users.noreply.github.com>
2022-12-09 14:31:20 +0100

    .

--- nuevo.ejercicio.recursividad.dam/src/main/java/nuevo/ejercicio/recursividad/dam/Principal.java
+++ nuevo.ejercicio.recursividad.dam/src/main/java/nuevo/ejercicio/recursividad/dam/Principal.java
@@ -1,11 +1,13 @@
 package nuevo.ejercicio.recursividad.dam;
 
+import java.util.Scanner;
+
 public class Principal {
 
 	public static void main(String[] args) {
 		// funciones.imprimeDivisores(2, 8904);
 
-		int posibleDivisor = 2;
+		/*int posibleDivisor = 2;
 		int leBuscoDivisores = 8094;
 
 		do {
@@ -15,7 +17,22 @@ public class Principal {
 			}
 			posibleDivisor++;
 		} while (posibleDivisor <= leBuscoDivisores / 2);
-
+	*/
+	
+		Scanner sc=new Scanner (System.in);
+		System.out.println("Introduce un texto");
+		String texto=sc.nextLine();
+		System.out.println("Y ahora una letra para buscarla en el texto");
+		char letra=sc.nextLine().charAt(0);
+		
+		byte contador=0;
+		for(short i=0;i<texto.length();i++) {
+			if(texto.charAt(i)==letra) {
+				contador++;
+			}
+		}
+		System.out.println(letra+" aparece "+ contador+" veces.");
 	}
+	
 
 }
