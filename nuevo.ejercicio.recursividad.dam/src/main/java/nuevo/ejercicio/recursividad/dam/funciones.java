033978082ed94e47d4ef86ee08890a2ed691356c
marllanesgarcia <115066302+Daniel1998-cenec@users.noreply.github.com>
2022-12-09 14:30:59 +0100

    .

--- nuevo.ejercicio.recursividad.dam/src/main/java/nuevo/ejercicio/recursividad/dam/funciones.java
+++ nuevo.ejercicio.recursividad.dam/src/main/java/nuevo/ejercicio/recursividad/dam/funciones.java
@@ -1,16 +1,20 @@
 package nuevo.ejercicio.recursividad.dam;
 
-public class funciones {
+public class Funciones {
 	public static void imprimeDivisores(int posibleDivisor, int leBuscoDivisores) {
-	if (posibleDivisor<=leBuscoDivisores/2) {//caso recursivo (iterativo)
-		if(leBuscoDivisores%posibleDivisor==0) {
-			System.out.println(posibleDivisor+" es divisor de "+leBuscoDivisores);
+		if (posibleDivisor <= leBuscoDivisores / 2) {// caso recursivo (iterativo)
+			if (leBuscoDivisores % posibleDivisor == 0) {
+				System.out.println(posibleDivisor + " es divisor de " + leBuscoDivisores);
+			}
+
+			imprimeDivisores(posibleDivisor + 1, leBuscoDivisores);
+		} else {
+
 		}
-		
-		imprimeDivisores(posibleDivisor+1,leBuscoDivisores);
-	}else {
-		
 	}
+
+	public static byte cuentraLetras(short i ) {
+		
 	}
 
 }
