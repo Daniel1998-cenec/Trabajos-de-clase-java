32b7304e3c029bf1f03d396d4835af6e6d77876b
Daniel1998-cenec <danielcesar1998@hotmail.com>
2023-02-03 11:19:08 +0100

    .

--- 2 trimestre/examen.subir.nota.primertrimetre.dam/src/main/java/examen/subir/nota/primertrimetre/dam/principal.java
+++ 2 trimestre/examen.subir.nota.primertrimetre.dam/src/main/java/examen/subir/nota/primertrimetre/dam/principal.java
@@ -1,9 +1,11 @@
 package examen.subir.nota.primertrimetre.dam;
 
+import java.util.Scanner;
+
 public class principal {
 
 	public static void main(String[] args) {
-		System.out.println("hola examen");
+		Scanner sc = new Scanner(System.in); 
 
 	}
 
