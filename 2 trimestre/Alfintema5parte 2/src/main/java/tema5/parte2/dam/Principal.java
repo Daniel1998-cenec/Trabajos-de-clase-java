8f56c7fe9ac91732b9db8973843c938a4ed7b705
Daniel1998-cenec <danielcesar1998@hotmail.com>
2023-02-19 18:31:56 +0100

    .

--- 2 trimestre/Alfintema5parte 2/src/main/java/tema5/parte2/dam/Principal.java
+++ 2 trimestre/Alfintema5parte 2/src/main/java/tema5/parte2/dam/Principal.java
@@ -1,11 +1,15 @@
 package tema5.parte2.dam;
 import clases.Comida;
+import clases.Perro;
 public class Principal {
 
 	public static void main(String[] args) {
-		Comida sopaPollo=new Comida("Sopa de pollo",(byte)40);
+		/*Comida sopaPollo=new Comida("Sopa de pollo",(byte)40);
 		
-		Comida soplaPollo= new Comida("Sopa de pollo",(byte)40);
+		Comida soplaPollo= new Comida("Sopa de pollo",(byte)40);*/
+		
+		Perro rubi=new Perro ("Rubi",'h',(byte)50,(byte)50,(byte)50,(byte)50,"canina",
+				"liso","Dani");
 	}
 
 }
